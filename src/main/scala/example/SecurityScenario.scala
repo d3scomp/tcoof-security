efc1993f1452e6891f00e4c1151e93cb0b30afa8
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2018-03-08 14:34:34 +0100

    Changed scenario map, added PersonState, capacity to rooms

--- src/main/scala/example/SecurityScenario.scala
+++ src/main/scala/example/SecurityScenario.scala
@@ -10,7 +10,7 @@ class SecurityScenario extends Model {
 
   class Building(val rooms: Set[Room], val doors: Set[Door]) extends Component
 
-  class Room(name: String) extends Component {
+  class Room(name: String, val capacity: Int) extends Component {
     name(name)
   }
 
@@ -37,8 +37,13 @@ class SecurityScenario extends Model {
     val TeamA, TeamB = Value
   }
 
+  object PersonState extends Enumeration {
+    val HeadingToLunch, HeadingToWorkplace, Leaving, Idle = Value
+  }
+
   class Person(name: String, val roles: List[Role.Value], var position: Room, val doors: Set[Door]) extends Component {
     name(name)
+    var mode: PersonState.Value = PersonState.Idle
 
     def act(): Unit = {
       val doorsFromRoom = doors.filter(_.srcRoom == position)
@@ -94,13 +99,40 @@ class SecurityScenario extends Model {
 
   class PersonToEnterTheRoom(val person: Person, val room: Room) extends RootEnsemble {
     val personsInRoom = new PersonsInRoom(room)
-    val toAvoid = avoidRoles.filter(_._1 == room).map(x => new AvoidRoles(x._1, x._2, x._3))
+
+    // all avoidance rules which apply to given room and roles of the given person
+//    val toAvoid = ensembles("RolesInRoomToAvoid", avoidRoles.map(x => new AvoidRoles(x._1, x._2, x._3))
+//          .filter(r => r.room == room)
+//          .filter(r => person.roles.contains(r.personRole))
+//    )
+
+    val rolesInRoomToAvoid = avoidRoles.filter(x => x._1 == room)
+                                       .filter(x => person.roles.contains(x._2))
+                                       .map(_._3).toSet
+
+//    val rolesInRoom = personsInRoom.persons.
 
     membership(
       // TODO - add emergency mode
       // check whether person with its role can enter the room - need access to AvoidRoles rules
       // which are set from outside
-      ???
+
+      // all the roles of the persons in room
+//      personsInRoom.persons.allMembers.values.flatMap(_.roles).toSet
+//      (personsInRoom.persons.values.flatMap(_.roles).toSet
+//      intersect
+//      toAvoid.map(_.allMembers).map(_.toAvoid))
+//        .isEmpty
+
+//      rolesInRoomToAvoid
+
+
+      // roles to avoid
+      //toAvoid.allMembers.map(_.toAvoid)
+
+
+
+      true
     )
   }
 
@@ -119,45 +151,47 @@ object TestScenario {
 
     // Building has following map, rooms X (exterior) just simulates space outside of the building.
     //
-    //  +---------------+
-    //  |               |
-    //  |   +---+---+   |
-    //  | X   A   B |   |
-    //  |   +- -+- -+   |
-    //  |   | C   D     |
-    //  |   +---+---+   |
-    //  |               |
-    //  +---------------+
+    //  +----------------------+
+    //  |                      |
+    //  |   +----+----+----+   |
+    //  | X | L1 | L2 | L3 |   |
+    //  |   +- --+- --+- --+   |
+    //  |     C1   C2   C3 |   |
+    //  |   +- --+- --+- --+   |
+    //  |   | W1 | W2 | W3 |   |
+    //  |   +----+----+----+   |
+    //  |                      |
+    //  +-------------------- -+
     //
-    // Room A at the beginning contains 2 persons from TeamA
-    // Room D at the beginning contains 2 persons from TeamB
+    // Rooms marked as W* are working places with capacities 2 (W1), 3 (W2) and 4 (W3).
+    // Rooms marked as L* are lunch places with capacities 3 (L1), 3 (L2) and 3 (L3).
+    // Rooms C* are corridors without capacity.
+    // There are two teams A and B each with 4 members, goal is to schedule them
+    // into working and lunch places, people from one team should avoid people from
+    // the other one.
+    // Modes of the person: HEADING_TO_LUNCH, HEADING_TO_WORKPLACE, LEAVING, IDLE
 
-    val roomA = new scenario.Room("A")
-    val roomB = new scenario.Room("B")
-    val roomC = new scenario.Room("C")
-    val roomD = new scenario.Room("D")
-    val exterior = new scenario.Room("Exterior")
+    val lunchRooms = List((1, 3), (2, 3), (3, 3)).map{case (i, capacity) => new scenario.Room(s"L$i", capacity)}
+    val workingRooms = List((1, 2), (2, 3), (3, 4)).map{case (i, capacity) => new scenario.Room(s"W$i", capacity)}
+    val corridors = (1 to 3).map(i => new scenario.Room(s"C$i", Int.MaxValue))
+    val exterior = new scenario.Room("Exterior", Int.MaxValue)
 
-    // Note that roomX is not part of the building
-    val rooms = Set(roomA, roomB, roomC, roomD)
+    // Note that exterior is not part of the building
+    val rooms = (lunchRooms ++ workingRooms ++ corridors).toSet
 
-    val doors = Set(
-      new scenario.Door(roomA, roomB), new scenario.Door(roomB, roomA),
-      new scenario.Door(roomC, roomD), new scenario.Door(roomD, roomC),
-      new scenario.Door(roomA, roomC), new scenario.Door(roomC, roomA),
-      new scenario.Door(roomB, roomD), new scenario.Door(roomD, roomB),
-      new scenario.Door(exterior, roomA), new scenario.Door(roomA, exterior),
-      new scenario.Door(exterior, roomD), new scenario.Door(roomD, exterior)
-    )
+    val doors = List(
+      (lunchRooms(0), corridors(0)), (lunchRooms(1), corridors(1)), (lunchRooms(2), corridors(2)),
+      (workingRooms(0), corridors(0)), (workingRooms(1), corridors(1)), (workingRooms(2), corridors(2)),
+      (corridors(0), corridors(1)), (corridors(1), corridors(2)),
+      (exterior, corridors(0))
+    ).flatMap{case (r1, r2) => List(new scenario.Door(r1, r2), new scenario.Door(r2, r1))}.toSet
 
     val building = new scenario.Building(rooms, doors)
 
-    val adam = new scenario.Person("Adam", List(scenario.Role.TeamA), roomA, doors)
-    val alice = new scenario.Person("Alice", List(scenario.Role.TeamA), roomA, doors)
-    val bob = new scenario.Person("Bob", List(scenario.Role.TeamB), roomD, doors)
-    val brenda = new scenario.Person("Brenda", List(scenario.Role.TeamB), roomD, doors)
+    val teamA = (1 to 4).map(i => new scenario.Person(s"Person A$i", List(scenario.Role.TeamA), exterior, doors))
+    val teamB = (1 to 4).map(i => new scenario.Person(s"Person B$i", List(scenario.Role.TeamB), exterior, doors))
 
-    val persons = List(adam, alice, bob, brenda)
+    val persons = teamA ++ teamB
 
     scenario.components = List(building) ++ persons
 
